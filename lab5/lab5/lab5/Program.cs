33b0923ff966e109f422580a054cfa3ef2a77822
bakkaSansanBakka <polina.potapeyko@mail.ru>
2020-11-11 20:27:27 +0300

    lab5, Program.cs: ToString() are called by different types of objects

--- lab5/lab5/lab5/Program.cs
+++ lab5/lab5/lab5/Program.cs
@@ -12,8 +12,11 @@ namespace lab5
         {
             Document document1 = new Document("ООО \"Белгород\"", 11, 11, 2020, true);
             document1.Burning();    // вызывается метод абстрактного класса
-            ((IManipulate)document1).Burning(); //вызывается метод интерфейса
+            ((IManipulate)document1).Burning(); // вызывается метод интерфейса
+            Console.WriteLine(document1.ToString());
 
+            Receipt receipt1 = new Receipt("ООО \"Белгород\"", 12, 12, 2020, true, "квитанция");
+            Console.WriteLine(receipt1.ToString());
             Console.ReadKey();
         }
     }
