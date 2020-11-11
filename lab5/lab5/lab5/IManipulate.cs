a27d47a9ef091515767dd1347df5d6f92dd94692
bakkaSansanBakka <polina.potapeyko@mail.ru>
2020-11-11 18:04:38 +0300

    lab5 interface file IManipulate.cs added

---
+++ lab5/lab5/lab5/IManipulate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    interface IManipulate
+    {
+        void Change();
+        void Find();
+        void Lose();
+    }
+}
