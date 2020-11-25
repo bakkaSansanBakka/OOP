4bf02449711fd67eaa6ef2d97b9b93e05d5efee3
bakkaSansanBakka <polina.potapeyko@mail.ru>
2020-11-25 22:29:55 +0300

    lab7 added

---
+++ lab7/lab7/lab7/Exceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab7
+{
+    public class PriceIsLessThanZeroException : ArgumentException
+    {
+        public PriceIsLessThanZeroException(string message) 
+            : base (message)
+        {
+
+        }
+    }
+    public class InvalidDocTypeException : ApplicationException
+    {
+        public InvalidDocTypeException(string message)
+            : base(message)
+        {
+
+        }
+    }
+    public class DateException : ApplicationException
+    {
+        public DateException(string message)
+            : base(message)
+        {
+
+        }
+    }
+    public class NullReferenceException : ApplicationException
+    {
+        public NullReferenceException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
